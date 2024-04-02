3272bcb79bd5bb2879a3c160c0a1f434a62d5807
Abdulfatai Buari <abuari@sidmach.com>
2024-04-02 07:17:44 +0100

    Using Prisma, shadecn form,zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+
+import ModalProvider from "@/providers/modal-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,9 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
